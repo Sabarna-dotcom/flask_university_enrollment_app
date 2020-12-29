76302e40dbf2509a04c5e9ca1400e384d6f1833c
Sabarna-dotcom <rssarkar26@gmail.com>
2020-12-29 16:18:29 +0530

    first version

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or b'6\xe9\xda\xead\x81\xf7\x8d\xbbH\x87\xe8m\xdd3%'
+
+    MONGODB_SETTINGS = { 'db' : 'UTA_Enrollment' ,
+    'host':'mongodb+srv://admin:admin@mydb.8003h.mongodb.net/UTA_Enrollment?retryWrites=true&w=majority'
+    
+    }
+
+
+    
